0a2baa955ad0dd309db17b413c14f2d3ec006f7b
leemember <hyunmail521@gmail.com>
2021-06-28 18:06:16 +0900

    action 생성과 리듀서

--- 01.redux/index.js
+++ 01.redux/index.js
@@ -1,11 +1,21 @@
-const { createStore } = require('redux');
+const { createStore } = require("redux");
 const reducer = () => {};
 const initialState = {
-  compA:'a',
-  compB:12,
-  compC:null,
-}
+  compA: "a",
+  compB: 12,
+  compC: null,
+};
 
-const store = createStore(reducer);
+const store = createStore(reducer, initialState);
 
 console.log(store.getState());
+
+//액션 = 액션은 그냥 객체다
+const changeCompA = (data) => {
+  return {
+    type: "CHANGE_COMP_A",
+    data,
+  };
+};
+
+store.dispatch(changeCompA("b"));
