0f328009a3acea523128cefe5230feab14f94840
leemember <hyunmail521@gmail.com>
2021-07-05 19:13:03 +0900

    리덕스 미들웨어 세팅

--- 01.redux/index2.js
+++ 01.redux/index2.js
@@ -1,4 +1,4 @@
-const { createStore } = require("redux");
+const { createStore, applyMiddleware } = require("redux");
 const reducer = require("./reducers");
 const { addPost } = require("./actions/post");
 const { logIn, logOut } = require("./actions/user");
@@ -16,7 +16,38 @@ const initialState = {
   followers: [],
 };
 
-const store = createStore(reducer, initialState);
+//처음보는 애니까 firstMiddleware로 지정했다.
+//그리고 3단 고차함수가 생긴다. (store) => (next) => (action)
+//매개변수가 각각 하나씩 3개나 들어간다. 처음 보면 당황할텐데
+/*
+fucntion firstMiddlewrae(store) {
+
+  return function (next) {
+
+    return function (action) {
+
+    }
+  }
+}
+
+이렇게 함수들이 중첩된 것이라고 보면 된다.
+*/
+const firstMiddleware = (store) => (dispatch) => (action) => {
+  console.log("액션 로깅", action); //2.기능 추가가 된거다.
+  //디스패치 하기 전에는 여기다가 기능을 넣는다
+  dispatch(action); // 1.기본 기능 전에
+  //디스패치 한 후에다가는 여기다가 기능을 넣는다
+  console.log("액션 끝");
+};
+
+// const enhancer = compose(applyMiddleware(), devtool);
+//여기서 사용한 compose는 combineReducer와 같은 합성해주는 역할을 한다.
+//만약 devtools랑 여러가지를 같이 사용할 때 compose를 사용하면 좋다.
+//하지만 우리는 devtools를 사용하지 않을 것이기 때문에
+const enhancer = applyMiddleware(firstMiddleware);
+//이렇게 까지만 해주어도 된다. 그리고 미들웨어는 디스패치와 리듀서 사이에 있는 것을 뜻한다.
+
+const store = createStore(reducer, initialState, enhancer);
 store.subscribe(() => {
   console.log("changed");
 });
